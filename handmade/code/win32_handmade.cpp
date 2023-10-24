cf1dd58107e3b05c317b4f19e8cb6546e82f3d95
TalKasi <kitalkasi@gmail.com>
2023-10-24 22:44:11 +0300

    Opening a Win32 Window

--- handmade/code/win32_handmade.cpp
+++ handmade/code/win32_handmade.cpp
@@ -1,7 +1,101 @@
 #include <windows.h>
 
-int CALLBACK WinMain(HINSTANCE instance, HINSTANCE prevInstance,
-                     LPSTR command_line, int show_code)
+LRESULT Wndproc(HWND Window, UINT Message,
+  WPARAM wParam, LPARAM lParam)
 {
+    LRESULT Result = 0;
+    switch(Message){
+    case WM_CLOSE:
+        {
+            OutputDebugStringA("WM_CLOSE\n");
+        } break;
+
+    case WM_DESTROY:
+        {
+            OutputDebugStringA("WM_DESTROY\n");
+        } break;
+
+    case WM_SIZE:
+        {
+            OutputDebugStringA("WM_SIZE\n");
+        } break;
+
+    case WM_ACTIVATEAPP:
+        {
+            OutputDebugStringA("WM_ACTIVATEAPP\n");
+        } break;
+
+    case WM_PAINT:
+        {
+            PAINTSTRUCT Paint;
+
+            HDC DeviceContext = BeginPaint(Window, &Paint);
+            int X = Paint.rcPaint.left;
+            int Y = Paint.rcPaint.top;
+            int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+            int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+
+            static DWORD Color = WHITENESS;
+
+            PatBlt(DeviceContext, X, Y, Width, Height, Color);
+
+            if (Color == WHITENESS)
+                Color = BLACKNESS;
+            else
+                Color = WHITENESS;
+
+            EndPaint(Window, &Paint);
+        } break;
+
+    default:
+        {
+//            OutputDebugStringA("default");
+            Result = DefWindowProcA(Window, Message, wParam, lParam);
+        }
+    }
+
+    return Result;
+}
+
+int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance,
+                     LPSTR CommandLine, int ShowCode)
+{   
+    WNDCLASS WindowClass = {};
+    WindowClass.style = CS_OWNDC|CS_VREDRAW|CS_HREDRAW;
+    WindowClass.lpfnWndProc = Wndproc;
+    WindowClass.hInstance = Instance;
+//    WindowClass.hIcon;
+    WindowClass.lpszClassName = "HandmadeGameWindowClass";
+
+    if (RegisterClassA(&WindowClass)) {
+        HWND WindowHandle = CreateWindowEx(0, WindowClass.lpszClassName, "HandmadeHero", WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+                  CW_USEDEFAULT, CW_USEDEFAULT,
+                  CW_USEDEFAULT, CW_USEDEFAULT, 
+                  0, 0, Instance, 0);
+
+        if (WindowHandle) {
+
+            MSG Message;
+            for (;;) {
+                BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+                if (MessageResult > 0) {
+                    TranslateMessage(&Message);
+                    DispatchMessageA(&Message);
+                }
+                else {
+                    break;
+                }
+            }
+        }
+        else
+        {
+            // TODO(Talkasi): Loggining
+        }
+    }
+    else 
+    {
+        // TODO(Talkasi): loggining
+    }
+
     return 0;
 }
